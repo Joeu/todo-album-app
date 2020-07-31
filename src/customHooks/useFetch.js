56df8cb7db8cfefdd46fa6bbdae84c02a86953d1
Joeumar Souza <joeumar@gmail.com>
2020-07-31 13:59:47 -0300

    Fetching data with custom hook

---
+++ src/customHooks/useFetch.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+export const useFetch = (url, opts) => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch(url, opts)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        setResponse(data);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, [url]);
+  return [response, loading, error];
+};
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useFetch } from '../customHooks/useFetch';
 
 const Posts = () => {
-  return <div>POSTS</div>;
+  const [response, loading, hasError] = useFetch(
+    'https://jsonplaceholder.typicode.com/posts/'
+  );
+
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : hasError ? (
+        <div>Error occured.</div>
+      ) : (
+        response?.map((post) => <div>{post.title}</div>)
+      )}
+    </>
+  );
 };
 
 export default Posts;
